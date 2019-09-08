5e87c013fa81f6332f0a16354c74837b8a715080
Mauro Zegarra <mauro.zegarra@gmail.com>
2019-09-08 10:40:20 -0500

    System Clock and Simple Timer working

--- app/src/main/java/com/maurozegarra/brainfocusx/TitleFragment.kt
+++ app/src/main/java/com/maurozegarra/brainfocusx/TitleFragment.kt
@@ -2,7 +2,6 @@ package com.maurozegarra.brainfocusx
 
 import android.os.Bundle
 import android.os.CountDownTimer
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,105 +9,82 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.maurozegarra.brainfocusx.databinding.FragmentTitleBinding
-import java.text.SimpleDateFormat
+import timber.log.Timber
 import java.util.*
 
 class TitleFragment : Fragment() {
 
+    // todo: add type of interval: work or rest
+    data class Interval(var start: Int, var end: Int)
+
+    private val allIntervals = listOf(
+        Interval(0, 25),
+        Interval(25, 30),
+        Interval(30, 55),
+        Interval(55, 0)
+    )
+
+    private var currentInterval = allIntervals[0]
+
     companion object {
-        // These represent different important times
-        // This is when the game is over
-        const val DONE = 0L
         // This is the number of milliseconds in a second
         const val ONE_SECOND = 1000L
-        // This is the total time of the game
-        const val COUNTDOWN_TIME = 10000L
-
         const val DAY = 1000L * 60L * 60L * 24L
-        const val HOUR = 1000L * 60L * 60L
-        const val ONE_MINUTE = 1000L * 60L
-        const val POMO1 = 25
-        const val POMO2 = 55
     }
 
     lateinit var timer: CountDownTimer
-    var currentTime: Long = 0L
+
+    // Contains all the views
+    private lateinit var binding: FragmentTitleBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding: FragmentTitleBinding = DataBindingUtil.inflate(
-            inflater, R.layout.fragment_title, container, false
-        )
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
 
         binding.buttonWork.setOnClickListener(
             Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_sessionFragment)
         )
 
-        ////////////////////////////////////////////////////////////////////////////////////////////
-        // setear el reloj a una hora específica \\
-        // obtiene una instancia del Calendar
-        var calendar = Calendar.getInstance()
-        // fija el calendar a las 4 horas
-        // HOUR is used for the 12-hour clock
-        calendar.set(Calendar.HOUR, 5)
-        // fija el calendar a los 35 minutos
-        calendar.set(Calendar.MINUTE, 15)
-        // fija el calendar a los 0 segundos
-        calendar.set(Calendar.SECOND, 0)
-        // obtiene el tiempo fijado en millis
-        var futureTime = calendar.timeInMillis
-        Log.i("onCreateView", "futureTime: $futureTime")
-
-        var futureTimeFormatted = convertLongToDateString(futureTime)
-        Log.i("onCreateView", "futureTimeFormatted: $futureTimeFormatted")
-
-        currentTime = System.currentTimeMillis()
-        var diffTime = futureTime - currentTime
-        Log.i("onCreateView", "currentTime: $currentTime, futureTime: $futureTime")
-        Log.i("onCreateView", "diffTime: $diffTime")
-
-        timer = object : CountDownTimer(diffTime, ONE_SECOND) {
+        timer = object : CountDownTimer(DAY, ONE_SECOND) {
 
             override fun onTick(millisUntilFinished: Long) {
-                currentTime = System.currentTimeMillis()
-                Log.i("onCreateView: Timer", "diffTime: $diffTime")
-                Log.i("onCreateView: Timer", "millisUntilFinished: $millisUntilFinished")
+                // Muestra el reloj del sistema
+                val now = Calendar.getInstance()
 
-                // muestra el reloj del sistema
-                var formattedTime = convertLongToDateString(currentTime)
-                binding.textTime.text = formattedTime
-                //Log.i("onCreateView", "system time: $formattedTime")
+                var hour = now.get(Calendar.HOUR)
+                if (hour == 0) {
+                    hour = 12
+                }
+                //Timber.i("$hour")
+
+                val minute = now.get(Calendar.MINUTE)
+                //val minute = 0
+                val second = now.get(Calendar.SECOND)
+
+                binding.textTime.text =
+                    String.format(Locale.getDefault(), "%2d:%02d:%02d", hour, minute, second)
 
                 // Muestra el Timer
-                var minutes = SimpleDateFormat("m").format(currentTime).toInt()
-                var seconds = SimpleDateFormat("s").format(currentTime).toInt()
+                getCurrentStartInterval(minute)
+                var timerMinute = currentInterval.end - minute - 1
+                //Timber.i("timerMinute: $timerMinute, currentInterval: $currentInterval, minute: $minute")
 
-                var timerMinutes = POMO2 - minutes - 1
-                var timerSeconds: Int
+                val timerSecond: Int
 
-                if (seconds == 0) {
-                    timerSeconds = 0
-                    timerMinutes += 1
+                if (second == 0) {
+                    timerSecond = 0
+                    timerMinute += 1
                 } else {
-                    timerSeconds = 60 - seconds
+                    timerSecond = 60 - second
                 }
 
-                binding.textTimer.text = String.format(
-                    Locale.getDefault(), "%2d:%02d", timerMinutes, timerSeconds
-                )
+                binding.textTimer.text =
+                    String.format(Locale.getDefault(), "%2d:%02d", timerMinute, timerSecond)
             }
 
-            override fun onFinish() {
-                currentTime = DONE
-                // fix para que el tiempo mostrado sea el tiempo fijado
-                // ejemplo tiempo fijado 4:55:00
-                // antes del fix se muestra 4:54:59
-                binding.textTime.text = convertLongToDateString(futureTime)
-                // fix para que el timer no se quede en 0:01
-                binding.textTimer.text = String.format(Locale.getDefault(), "%2d:%02d", 0, 0)
-            }
+            override fun onFinish() {}
         }
 
         timer.start()
@@ -116,14 +92,29 @@ class TitleFragment : Fragment() {
         return binding.root
     }
 
-    fun convertLongToDateString(systemTime: Long): String {
-        return SimpleDateFormat("h:mm:ss").format(systemTime).toString()
-    }
+    private fun getCurrentStartInterval(currentMinute: Int) {
+        //Timber.i("$currentMinute")
+        if (currentMinute == 55) {
+            currentInterval.start = 55
+            currentInterval.end = 60
+        }
 
-    fun formatTimer(timeInMillis: Long): String {
-        val minutes = (timeInMillis / 1000) / 60
-        val seconds = (timeInMillis / 1000) % 60
+        var newInterval = allIntervals[0]
 
-        return String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds)
+        for (interval in allIntervals) {
+            //Timber.i("${interval.start}, ${interval.end}")
+            if (interval.start <= currentMinute && currentMinute < interval.end) {
+                //Timber.i("$interval")
+                newInterval = interval
+                break
+            }
+        }
+
+        // If the new start interval is actually different than the current start interval
+        if (newInterval != currentInterval) {
+            currentInterval = newInterval
+            //binding.dessertButton.setImageResource(newDessert.imageId)
+        }
+        //Timber.i("currentInterval: $currentInterval")
     }
 }
